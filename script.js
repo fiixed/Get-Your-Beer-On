6f3db45d16a819dc2eb3d0a4e9fff27e40dcb382
fiixed <fiixed@gmail.com>
2020-10-19 09:58:35 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let brewerieSearchData;
+
+document.addEventListener('DOMContentLoaded', () => {
+    function renderBreweries(brewerieArray) {
+        let brewerieHTML = brewerieArray.map(currentBrewerie => {
+            return `
+                <div class="brewerieCard">
+                    <a href="${currentBrewerie.website_url}">${currentBrewerie.name}</a><br>
+                    <p>${currentBrewerie.city}, ${currentBrewerie.state} ${currentBrewerie.postal_code}</p><br>
+                </div>
+            `;
+        });
+        return brewerieHTML.join('');
+    };
+    document.getElementById('search-form').addEventListener('submit', e => {
+        e.preventDefault();
+        let searchString = document.querySelector('.search-bar').value;
+        let urlEncodedSearchString = encodeURIComponent(searchString);
+        axios.get(`https://api.openbrewerydb.org/breweries/search?query=${urlEncodedSearchString}`).then(response => {
+            brewerieSearchData = response.data;
+            document.querySelector(".breweries-container").innerHTML = renderBreweries(brewerieSearchData);
+        });
+    });
+});
