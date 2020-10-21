014e5f4e9663c1849128420c4e13c508af9f58c9
fiixed <fiixed@gmail.com>
2020-10-20 20:38:11 -0400

    seperated out lat/lon

--- scripts/geo.js
+++ scripts/geo.js
@@ -1,9 +1,11 @@
 let geoKey = config.GEO;
+let lat = '33.812500';
+let lon = '-84.424942';
 
 document.addEventListener('DOMContentLoaded', () => {
   (async () => {
     try {
-      const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=${geoKey}`);
+      const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${geoKey}`);
       console.log(response.data);
       
     } catch (error) {
