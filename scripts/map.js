78feaeb23ef18139731c51ac76df6ce33ffeb303
fiixed <fiixed@gmail.com>
2020-10-21 18:35:27 -0400

    dynamic map

--- scripts/map.js
+++ scripts/map.js
@@ -1,24 +1,24 @@
 let mapKey = config.GEO;
 
-// Create the script tag, set the appropriate attributes
-var script = document.createElement('script');
-script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyA1IUHJ6maXXRvBCQ6FPKPbQUpngPkqAoM&callback=initMap`;
-script.defer = true;
-
-
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
+let map;
+const center = { lat: 33.812500, lng: -84.424942 };
+const zoom = 14;
+const url = "https://maps.googleapis.com/maps/api/staticmap";
+const loader = new google.maps.plugins.loader.Loader({
+  apiKey: mapKey,
+  version: "weekly",
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const wrapper = document.getElementById("wrapper");
+  wrapper.style.backgroundImage = `url(${url}?center=${center.lat},${center.lng}&zoom=${zoom}&scale=2&size=${wrapper.clientWidth}x${wrapper.clientHeight}&key=YOUR_API_KEY)`;
+  wrapper.addEventListener("click", () => {
+    wrapper.remove();
+    loader.load().then(() => {
+      map = new google.maps.Map(document.getElementById("map"), {
+        center,
+        zoom,
+      });
     });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
+  });
+});
 
