30ac77fd0874d2c327e5cf1e86f4a35fbe179154
JasonWilger <jkw500@gmail.com>
2020-10-28 12:02:02 -0400

    fixed favicon file

--- scripts/map.js
+++ scripts/map.js
@@ -394,3 +394,120 @@ if (localStorage.getItem("theme") === "dark") {
   document.body.classList.add("dark");
 }
 //end of dark mode code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// map dark mode
+
+// button.addEventListener("click", () => {
+//   document.body.classList.toggle("dark");
+//   localStorage.setItem(
+//     "theme",
+//     document.body.classList.contains("dark") ? "dark" : "light"
+//   );
+// });
+
+// if (localStorage.getItem("initMap") === "darkMode") {
+//   document.body.classList.add("darkMode");
+// }
+
+// function initMap() {
+//   // Styles a map in night mode.
+//   const initMap = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: 40.674, lng: -73.945 },
+//     zoom: 12,
+//     styles: [
+//       { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+//       { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+//       { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+//       {
+//         featureType: "administrative.locality",
+//         elementType: "labels.text.fill",
+//         stylers: [{ color: "#d59563" }],
+//       },
+//       {
+//         featureType: "poi",
+//         elementType: "labels.text.fill",
+//         stylers: [{ color: "#d59563" }],
+//       },
+//       {
+//         featureType: "poi.park",
+//         elementType: "geometry",
+//         stylers: [{ color: "#263c3f" }],
+//       },
+//       {
+//         featureType: "poi.park",
+//         elementType: "labels.text.fill",
+//         stylers: [{ color: "#6b9a76" }],
+//       },
+//       {
+//         featureType: "road",
+//         elementType: "geometry",
+//         stylers: [{ color: "#38414e" }],
+//       },
+//       {
+//         featureType: "road",
+//         elementType: "geometry.stroke",
+//         stylers: [{ color: "#212a37" }],
+//       },
+//       {
+//         featureType: "road",
+//         elementType: "labels.text.fill",
+//         stylers: [{ color: "#9ca5b3" }],
+//       },
+//       {
+//         featureType: "road.highway",
+//         elementType: "geometry",
+//         stylers: [{ color: "#746855" }],
+//       },
+//       {
+//         featureType: "road.highway",
+//         elementType: "geometry.stroke",
+//         stylers: [{ color: "#1f2835" }],
+//       },
+//       {
+//         featureType: "road.highway",
+//         elementType: "labels.text.fill",
+//         stylers: [{ color: "#f3d19c" }],
+//       },
+//       {
+//         featureType: "transit",
+//         elementType: "geometry",
+//         stylers: [{ color: "#2f3948" }],
+//       },
+//       {
+//         featureType: "transit.station",
+//         elementType: "labels.text.fill",
+//         stylers: [{ color: "#d59563" }],
+//       },
+//       {
+//         featureType: "water",
+//         elementType: "geometry",
+//         stylers: [{ color: "#17263c" }],
+//       },
+//       {
+//         featureType: "water",
+//         elementType: "labels.text.fill",
+//         stylers: [{ color: "#515c6d" }],
+//       },
+//       {
+//         featureType: "water",
+//         elementType: "labels.text.stroke",
+//         stylers: [{ color: "#17263c" }],
+//       },
+//     ],
+//   });
+// }
+
+// end map dark mode
