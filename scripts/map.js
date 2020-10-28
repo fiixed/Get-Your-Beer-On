d7a66f83959db09dc18b4a00761092ddcd54be1e
JasonWilger <jkw500@gmail.com>
2020-10-28 16:11:20 -0400

    enabled dark mode for google maps

--- scripts/map.js
+++ scripts/map.js
@@ -12,6 +12,7 @@ function initMap() {
     zoom: 4,
     minZoom: 1,
   });
+
   var geocoder = new google.maps.Geocoder();
   geocoder.geocode({ address: country }, function (results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
@@ -20,6 +21,7 @@ function initMap() {
       swal("Could not find location: " + location);
     }
   });
+
   const locationButton = document.getElementById("location");
   locationButton.classList.add("custom-map-control-button");
   locationButton.addEventListener("click", () => {
@@ -380,35 +382,105 @@ const titleCase = (str) => {
   return splitStr.join(" ");
 };
 
-//dark mode code
+// map dark mode
+document.getElementById('darkMode1').addEventListener('change', function(event){
 
-button.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  localStorage.setItem(
-    "theme",
-    document.body.classList.contains("dark") ? "dark" : "light"
-  );
+  (event.target.checked) ? dark() : initMap();
+
 });
 
-if (localStorage.getItem("theme") === "dark") {
-  document.body.classList.add("dark");
-}
-//end of dark mode code
+function dark() {
+  
+  let myStyle = {
 
+    styles: [
+      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [{ color: "#263c3f" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#6b9a76" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#38414e" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#212a37" }],
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#9ca5b3" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#746855" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#1f2835" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#f3d19c" }],
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [{ color: "#2f3948" }],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ color: "#17263c" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#515c6d" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#17263c" }],
+      },
+    ],
+  };
 
+  map.setOptions(myStyle);
 
+};
+// end map dark mode
 
-
-
-
-
-
-
-
-
-
-
-// map dark mode
+//dark mode code
 
 // button.addEventListener("click", () => {
 //   document.body.classList.toggle("dark");
@@ -418,96 +490,6 @@ if (localStorage.getItem("theme") === "dark") {
 //   );
 // });
 
-// if (localStorage.getItem("initMap") === "darkMode") {
-//   document.body.classList.add("darkMode");
+// if (localStorage.getItem("theme") === "dark") {
+//   document.body.classList.add("dark");
 // }
-
-// function initMap() {
-//   // Styles a map in night mode.
-//   const initMap = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: 40.674, lng: -73.945 },
-//     zoom: 12,
-//     styles: [
-//       { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-//       { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-//       { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-//       {
-//         featureType: "administrative.locality",
-//         elementType: "labels.text.fill",
-//         stylers: [{ color: "#d59563" }],
-//       },
-//       {
-//         featureType: "poi",
-//         elementType: "labels.text.fill",
-//         stylers: [{ color: "#d59563" }],
-//       },
-//       {
-//         featureType: "poi.park",
-//         elementType: "geometry",
-//         stylers: [{ color: "#263c3f" }],
-//       },
-//       {
-//         featureType: "poi.park",
-//         elementType: "labels.text.fill",
-//         stylers: [{ color: "#6b9a76" }],
-//       },
-//       {
-//         featureType: "road",
-//         elementType: "geometry",
-//         stylers: [{ color: "#38414e" }],
-//       },
-//       {
-//         featureType: "road",
-//         elementType: "geometry.stroke",
-//         stylers: [{ color: "#212a37" }],
-//       },
-//       {
-//         featureType: "road",
-//         elementType: "labels.text.fill",
-//         stylers: [{ color: "#9ca5b3" }],
-//       },
-//       {
-//         featureType: "road.highway",
-//         elementType: "geometry",
-//         stylers: [{ color: "#746855" }],
-//       },
-//       {
-//         featureType: "road.highway",
-//         elementType: "geometry.stroke",
-//         stylers: [{ color: "#1f2835" }],
-//       },
-//       {
-//         featureType: "road.highway",
-//         elementType: "labels.text.fill",
-//         stylers: [{ color: "#f3d19c" }],
-//       },
-//       {
-//         featureType: "transit",
-//         elementType: "geometry",
-//         stylers: [{ color: "#2f3948" }],
-//       },
-//       {
-//         featureType: "transit.station",
-//         elementType: "labels.text.fill",
-//         stylers: [{ color: "#d59563" }],
-//       },
-//       {
-//         featureType: "water",
-//         elementType: "geometry",
-//         stylers: [{ color: "#17263c" }],
-//       },
-//       {
-//         featureType: "water",
-//         elementType: "labels.text.fill",
-//         stylers: [{ color: "#515c6d" }],
-//       },
-//       {
-//         featureType: "water",
-//         elementType: "labels.text.stroke",
-//         stylers: [{ color: "#17263c" }],
-//       },
-//     ],
-//   });
-// }
-
-// end map dark mode
